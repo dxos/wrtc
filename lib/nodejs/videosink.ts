7f17eeff4f8dcc844f2510f61b64a3dce9f78eef
William Lahti <wilahti@gmail.com>
2022-08-06 15:08:59 -0700

    better define type of RTCVideoSink

--- lib/nodejs/videosink.ts
+++ lib/nodejs/videosink.ts
@@ -1,7 +1,17 @@
 import { inherits } from 'util';
 import * as native from '../../binding';
 import { EventTarget } from './eventtarget';
-export const RTCVideoSink = native.RTCVideoSink;
+export const RTCVideoSink: typeof RTCVideoSinkT = native.RTCVideoSink;
 export type RTCVideoSink = typeof RTCVideoSinkT;
-declare class RTCVideoSinkT { }
+
+export interface RTCVideoSinkEvent extends Event {
+    frame: any;
+}
+
+declare class RTCVideoSinkT extends EventTarget {
+    constructor(track: MediaStreamTrack);
+    stop(): void;
+    readonly stopped: boolean;
+    onframe: (ev: RTCVideoSinkEvent) => void;
+}
 inherits(native.RTCVideoSink, EventTarget);
