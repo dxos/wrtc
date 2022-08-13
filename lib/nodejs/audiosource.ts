6c332b3dc15b4d2920eb15ca7edf07f0ef69169a
William Lahti <wilahti@gmail.com>
2022-08-13 00:16:01 -0700

    proper types for RTCAudioData

--- lib/nodejs/audiosource.ts
+++ lib/nodejs/audiosource.ts
@@ -1,4 +1,15 @@
 import * as native from '../../binding';
-export const RTCAudioSource = native.RTCAudioSource;
-export type RTCAudioSource = typeof RTCAudioSourceT;
-declare class RTCAudioSourceT { }
+export const RTCAudioSource: typeof RTCAudioSourceT = native.RTCAudioSource;
+export type RTCAudioSource = RTCAudioSourceT;
+
+export interface RTCAudioData {
+  samples: Int16Array;
+  sampleRate: number;
+  bitsPerSample?: number;
+  channelCount?: number;
+  numberOfFrames?: number;
+}
+declare class RTCAudioSourceT { 
+    createTrack(): MediaStreamTrack;
+    onData(data: RTCAudioData): void;
+}
