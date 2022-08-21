fac7c456244b1e796321b39fc91cf476420e6138
William Lahti <wilahti@gmail.com>
2022-08-21 15:07:25 -0700

    fixes for wpt server

--- lib/nodejs/test/web-platform-tests/start-wpt-server.ts
+++ lib/nodejs/test/web-platform-tests/start-wpt-server.ts
@@ -1,12 +1,13 @@
 'use strict';
 /* eslint-disable no-console, global-require */
-const path = require('path');
-const dns = require('dns');
-const childProcess = require('child_process');
-const q = require('q');
-const { inBrowserContext } = require('./util.js');
-const requestHead = require('request-promise-native').head;
+import path from 'path';
+import dns from 'dns';
+import childProcess from 'child_process';
+import q from 'q';
+import { inBrowserContext } from './util.js';
+import { head as requestHead } from 'request-promise-native';
 const dnsLookup = q.denodeify(dns.lookup);
+import which from 'which';
 
 const wptDir = path.resolve(__dirname, 'tests');
 
@@ -20,7 +21,7 @@ const configs = {
   toUpstream: require(configPaths.toUpstream)
 };
 
-export default ({ toUpstream = false } = {}) => {
+export default async ({ toUpstream = false } = {}) => {
   if (inBrowserContext()) {
     return Promise.resolve();
   }
@@ -31,45 +32,49 @@ export default ({ toUpstream = false } = {}) => {
 
   const urlPrefix = `http://${config.browser_host}:${config.ports.http[0]}/`;
 
-  return dnsLookup('web-platform.test').then(
-    () => {
-      const configArg = path.relative(path.resolve(wptDir), configPath);
-      const args = ['./wpt.py', 'serve', '--config', configArg];
-      const python = childProcess.spawn('python', args, {
-        cwd: wptDir,
-        stdio: 'inherit'
-      });
+  try {
+    await dnsLookup('web-platform.test');
+  } catch (e) {
+    throw new Error('Host entries not present for web platform tests. See ' +
+      'https://github.com/w3c/web-platform-tests#running-the-tests');
+  }
 
-      return new Promise((resolve, reject) => {
-        python.on('error', e => {
-          console.error(`Error starting python server process: ${e.message}`);
-          reject(new Error(`Error starting python server process: ${e.message}`));
-        });
+  const configArg = path.relative(path.resolve(wptDir), configPath);
+  const args = ['./wpt.py', 'serve', '--config', configArg];
 
-        resolve(pollForServer(urlPrefix));
+  let pythonPath = await which('python');
 
-        process.on('exit', () => {
-          // Python doesn't register a default handler for SIGTERM and it doesn't run __exit__() methods of context
-          // managers when it gets that signal. Using SIGINT avoids this problem.
-          python.kill('SIGINT');
-        });
-      });
-    },
-    () => {
-      throw new Error('Host entries not present for web platform tests. See ' +
-                      'https://github.com/w3c/web-platform-tests#running-the-tests');
-    }
-  );
+  console.log(`Starting WPT server...`);
+  const python = childProcess.spawn(pythonPath, args, {
+    cwd: wptDir,
+    stdio: 'inherit'
+  });
+
+  return new Promise((resolve, reject) => {
+    python.on('error', e => {
+      console.error(`Error starting python server process: ${e.message}`);
+      reject(new Error(`Error starting python server process: ${e.message}`));
+    });
+
+    resolve(pollForServer(urlPrefix));
+
+    process.on('exit', () => {
+      // Python doesn't register a default handler for SIGTERM and it doesn't run __exit__() methods of context
+      // managers when it gets that signal. Using SIGINT avoids this problem.
+      python.kill('SIGINT');
+    });
+  });
 };
 
-function pollForServer(url) {
-  return requestHead(url)
-    .then(() => {
-      console.log(`WPT server at ${url} is up!`);
-      return url;
-    })
-    .catch(err => {
-      console.log(`WPT server at ${url} is not up yet (${err.message}); trying again`);
-      return q.delay(500).then(() => pollForServer(url));
-    });
+async function pollForServer(url) {
+  try {
+    await requestHead(url);
+  } catch (err) {
+    console.log(`WPT server at ${url} is not up yet (${err.message}); trying again`);
+    await q.delay(500);
+    return pollForServer(url);
+  }
+
+  console.log(`WPT server at ${url} is up!`);
+  return url;
 }
